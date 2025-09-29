63de85ab1c413d8ea23d45ead3f13d81b1d507d5
Richard Draycott <rich@wikoion.dev>
2025-09-29 12:34:40 +0100

    feat: Update keymap

--- keyboards/sunder/c60he/keymaps/vial/keymap.c
+++ keyboards/sunder/c60he/keymaps/vial/keymap.c
@@ -1,18 +1,144 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,   \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,    \
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_NO,   KC_BSLS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,   \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,    \
-                                            KC_LCTL, KC_LGUI, KC_SPC,  KC_SPC,  MO(1),   KC_RALT                                        \
-    ),
-    [1] = LAYOUT(
-        KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_NO,     \
-        KC_NO,   KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     \
-        KC_CAPS, KC_NO,   KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_NO,   KC_NO,     \
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     \
-                                            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS, KC_NO                                          \
-    ),
+	[0] = LAYOUT(
+		KC_Q, KC_W, KC_E, KC_5, KC_4, KC_3, KC_2, KC_1, KC_GRV, KC_TAB,
+		KC_Z, KC_G, KC_T, KC_R, LSFT_T(KC_F), LCTL_T(KC_D), LALT_T(KC_S), LGUI_T(KC_A), KC_EQL, KC_LSFT,
+		KC_LGUI, MO(1), KC_SPC, KC_NO, KC_NO, KC_NO, KC_B, KC_V, KC_C, KC_X,
+		KC_Y, KC_U, KC_I, KC_0, KC_9, KC_8, KC_7, KC_6, KC_NO, KC_NO,
+		KC_N, RGUI_T(KC_SCLN), KC_P, KC_O, RALT_T(KC_L), RCTL_T(KC_K), RSFT_T(KC_J), KC_H, KC_NO, KC_ENT,
+		MO(1), KC_ESC, KC_BSPC, KC_MINS, KC_QUOT, KC_RSFT, KC_SLSH, KC_DOT, KC_COMM, KC_M
+	),
+	[1] = LAYOUT(
+		KC_F11, KC_F12, KC_NO, KC_F5, KC_F4, KC_F3, KC_F2, KC_F1, KC_NO, KC_NO,
+		KC_NO, KC_END, KC_NO, KC_NO, LSFT_T(KC_PGUP), LCTL_T(KC_PGDN), LALT_T(KC_HOME), LGUI_T(KC_NO), KC_CAPS, KC_LSFT,
+		KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		KC_NO, KC_NO, KC_NO, KC_F10, KC_F9, KC_F8, KC_F7, KC_F6, KC_NO, KC_NO,
+		KC_NO, KC_QUOT, KC_LBRC, KC_NO, RGUI_T(KC_RGHT), RALT_T(KC_UP), RCTL_T(KC_DOWN), RSFT_T(KC_LEFT), KC_NO, KC_NO,
+		KC_TRNS, KC_NO, KC_DEL, KC_RBRC, KC_NUHS, KC_NO, KC_NUBS, KC_NO, KC_NO, KC_NO
+	),
+	[2] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+	[3] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+	[4] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+	[5] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+	[6] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	),
+	[7] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	)
 };
+
+
+// -------------------- Per-key Tapping Term --------------------
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LSFT_T(KC_F):
+        case RSFT_T(KC_J):
+
+        case LSFT_T(KC_END):
+        case RSFT_T(KC_LEFT):
+            return 150;
+        default:
+            return 220; // fallback to global
+    }
+}
+// ----- Per-key IGNORE_MOD_TAP_INTERRUPT -----
+bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LSFT_T(KC_F):
+        case RSFT_T(KC_J):
+        case LCTL_T(KC_D):
+        case LALT_T(KC_S):
+        case LGUI_T(KC_A):
+        case RGUI_T(KC_SCLN):
+        case RALT_T(KC_L):
+        case RCTL_T(KC_K):
+
+        case LSFT_T(KC_END):
+        case RSFT_T(KC_LEFT):
+        case LCTL_T(KC_PGUP):
+        case LALT_T(KC_PGDN):
+        case LGUI_T(KC_NO):
+        case RGUI_T(KC_RIGHT):
+        case RALT_T(KC_UP):
+        case RCTL_T(KC_DOWN):
+            return true;
+        default:
+            return false;
+    }
+}
+
+// ----- Per-key HOLD_ON_OTHER_KEY_PRESS -----
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LSFT_T(KC_F):
+        case RSFT_T(KC_J):
+
+        case LSFT_T(KC_END):
+        case RSFT_T(KC_LEFT):
+            return true;
+        default:
+            return false;
+    }
+}
+
+// ----- Per-key PERMISSIVE_HOLD -----
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LCTL_T(KC_D):
+        case LALT_T(KC_S):
+        case LGUI_T(KC_A):
+        case RGUI_T(KC_SCLN):
+        case RALT_T(KC_L):
+        case RCTL_T(KC_K):
+
+        case LCTL_T(KC_PGUP):
+        case LALT_T(KC_PGDOWN):
+        case LGUI_T(KC_NO):
+        case RGUI_T(KC_RIGHT):
+        case RALT_T(KC_UP):
+        case RCTL_T(KC_DOWN):
+            return true;
+        default:
+            return false;
+    }
+}
