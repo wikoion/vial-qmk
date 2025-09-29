80c698942dee765c2f62c4ae6e4fe91a2d66d340
Richard Draycott <rich@wikoion.dev>
2025-09-29 08:46:28 -0700

    feat: Add wikion keymap, need to remember to restore vial

--- .ccls-cache/@Users@richarddraycott@git@vial-qmk/keyboards@sunder@c60he@keymaps@wikoion@keymap.c
+++ .ccls-cache/@Users@richarddraycott@git@vial-qmk/keyboards@sunder@c60he@keymaps@wikoion@keymap.c
@@ -2,20 +2,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        /* r0 */ KC_Q, KC_W, KC_E, KC_5, KC_4, KC_3, KC_2, KC_1, KC_GRAVE, KC_TAB,
-        /* r1 */ KC_Z, KC_G, KC_T, KC_R, LSFT_T(KC_F), LCTL_T(KC_D), LALT_T(KC_S), LGUI_T(KC_A), KC_EQUAL, KC_LSHIFT,
-        /* r2 */ KC_LGUI, MO(1), KC_SPACE, KC_NO, KC_NO, KC_NO, KC_B, KC_V, KC_C, KC_X,
-        /* r3 */ KC_Y, KC_U, KC_I, KC_0, KC_9, KC_8, KC_7, KC_6, KC_NO, KC_NO,
-        /* r4 */ KC_N, RGUI_T(KC_SCLN), KC_P, KC_O, RALT_T(KC_L), RCTL_T(KC_K), RSFT_T(KC_J), KC_H, KC_NO, KC_ENTER,
-        /* r5 */ MO(1), KC_ESCAPE, KC_BSPACE, KC_MINUS, KC_QUOTE, KC_RSHIFT, KC_SLASH, KC_DOT, KC_COMMA, KC_M
+        KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO, KC_NO, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,    KC_NO,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS,
+        KC_EQUAL,  LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_NO, KC_NO, KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOTE,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLASH, KC_RSFT,
+                                            KC_LGUI, MO(1),   KC_SPACE, KC_ENTER, MO(1), KC_ESCAPE
     ),
     [1] = LAYOUT(
-        /* r0 */ KC_F11, KC_F12, KC_NO, KC_F5, KC_F4, KC_F3, KC_F2, KC_F1, KC_NO, KC_NO,
-        /* r1 */ KC_NO, KC_END, KC_NO, KC_NO, LSFT_T(KC_PGUP), LCTL_T(KC_PGDOWN), LALT_T(KC_HOME), LGUI_T(KC_NO), KC_CAPS, KC_LSHIFT,
-        /* r2 */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-        /* r3 */ KC_NO, KC_NO, KC_NO, KC_F10, KC_F9, KC_F8, KC_F7, KC_F6, KC_NO, KC_NO,
-        /* r4 */ KC_NO, KC_QUOTE, KC_LBRACKET, KC_NO, RGUI_T(KC_RIGHT), RALT_T(KC_UP), RCTL_T(KC_DOWN), RSFT_T(KC_LEFT), KC_NO, KC_NO,
-        /* r5 */ KC_TRNS, KC_NO, KC_DELETE, KC_RBRACKET, KC_NONUS_HASH, KC_NO, KC_NONUS_BSLASH, KC_NO, KC_NO, KC_NO
+        KC_NO,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DELETE,
+        KC_NO,    KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC,
+        KC_CAPS,  LGUI_T(KC_NO), LALT_T(KC_HOME), LCTL_T(KC_PGDN), LSFT_T(KC_PGUP), KC_END, KC_NO, KC_NO, RSFT_T(KC_LEFT), RCTL_T(KC_DOWN), RALT_T(KC_UP), RGUI_T(KC_RIGHT), KC_QUOTE, KC_NUHS,
+        KC_LSFT,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSLS,   KC_NO,
+                                            KC_NO,   KC_NO, KC_NO,   KC_NO, KC_NO,   KC_NO
     ),
 	[2] = LAYOUT(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
--- keyboards/sunder/c60he/keymaps/wikoion/keymap.c
+++ keyboards/sunder/c60he/keymaps/wikoion/keymap.c
@@ -2,18 +2,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_EQUAL, KC_MINUS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,    KC_NO,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENTER,
-        KC_LSFT,  LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_NO, KC_NO, KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOTE,
+        KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO, KC_NO, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_NO,    KC_NO,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINUS,
+        KC_EQUAL,  LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G, KC_NO, KC_NO, KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOTE,
         KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMMA, KC_DOT,  KC_SLASH, KC_RSFT,
-                                            KC_LGUI, MO(1),   KC_SPACE, KC_BSPC,  KC_ESCAPE, MO(1)
+                                            KC_LGUI, MO(1),   KC_SPACE, KC_ENTER, MO(1), KC_ESCAPE
     ),
     [1] = LAYOUT(
-        KC_NO,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_RBRC,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DELETE,
-        KC_NO,    KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NONUS_HASH, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_NO,
-        KC_LSFT,  LGUI_T(KC_NO), LALT_T(KC_HOME), LCTL_T(KC_PGDN), LSFT_T(KC_PGUP), KC_END, KC_NO, KC_NONUS_BACKSLASH, RSFT_T(KC_LEFT), RCTL_T(KC_DOWN), RALT_T(KC_UP), RGUI_T(KC_RIGHT), KC_QUOTE, KC_NO,
-        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-                                            KC_NO,   KC_TRNS, KC_NO,   KC_DELETE, KC_NO,   KC_TRNS
+        KC_NO,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,  KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DELETE,
+        KC_NO,    KC_F11,  KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LBRC, KC_RBRC,
+        KC_CAPS,  LGUI_T(KC_NO), LALT_T(KC_HOME), LCTL_T(KC_PGDN), LSFT_T(KC_PGUP), KC_END, KC_NO, KC_NO, RSFT_T(KC_LEFT), RCTL_T(KC_DOWN), RALT_T(KC_UP), RGUI_T(KC_RIGHT), KC_QUOTE, KC_NUHS,
+        KC_LSFT,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSLS,   KC_NO,
+                                            KC_NO,   KC_NO, KC_NO,   KC_NO, KC_NO,   KC_NO
     ),
 	[2] = LAYOUT(
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
